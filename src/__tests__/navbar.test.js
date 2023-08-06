1c8ee0f1b4ded21071d787d004e75b3f60602837
kit0-0 <kt1767@gmail.com>
2023-08-07 05:14:56 +0530

    Add test for navbar

---
+++ src/__tests__/navbar.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter, BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+describe('Navbar Component', () => {
+  test('renders title correctly based on pathname', () => {
+    // Test case when pathname is '/'
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Navbar />
+      </MemoryRouter>,
+    );
+    const titleForRoot = getByText('Most Animes');
+    expect(titleForRoot).toBeInTheDocument();
+
+    // Test case when pathname is '/anime-details'
+    const { getByText: getByTextDetails } = render(
+      <MemoryRouter initialEntries={['/anime-details']}>
+        <Navbar />
+      </MemoryRouter>,
+    );
+    const titleForDetails = getByTextDetails('Anime Details');
+    expect(titleForDetails).toBeInTheDocument();
+  });
+
+  test('renders navbar', () => {
+    const { container } = render(
+      <Router>
+        <Navbar />
+      </Router>,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
