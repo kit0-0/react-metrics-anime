567e20dcc87a53f3b21f095b0c34cec6f3c02d3d
kit0-0 <kt1767@gmail.com>
2023-08-07 04:26:27 +0530

    Add anime reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import animeReducer from './anime/AnimeSlice';
 
 const store = configureStore({
   reducer: {
+    anime: animeReducer,
   },
 });
 
