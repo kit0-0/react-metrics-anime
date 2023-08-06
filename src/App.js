8aa57b3ad87d976fb490ee54094d5191717b3c51
kit0-0 <kt1767@gmail.com>
2023-08-07 04:26:05 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import AnimeCard from './components/AnimeCardComponent';
 import Navbar from './components/Navbar';
+import Details from './components/AnimeDetails';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/react-metrics-anime">
       <div className="App">
         <Navbar />
+        <div>
+          <Routes>
+            <Route exact path="/" element={<AnimeCard />} />
+            <Route path="/Details" element={<Details />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
