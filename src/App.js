d7dd571780fd81f3a83b357438170ab49be8242f
kit0-0 <kt1767@gmail.com>
2023-08-07 03:10:24 +0530

    import nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
